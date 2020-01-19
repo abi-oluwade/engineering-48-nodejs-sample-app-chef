1ae2e6512b08905fc29dfda8d723c80685f76c84
Abi Oluwade <abi.oluwade1@gmail.com>
2020-01-19 11:27:32 +0000

    updated recipe with working nginx

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,20 +5,35 @@
 # Copyright:: 2020, The Authors, All Rights Reserved.
 
 # imports and installs the node.js dependencies and libraries specified in the metadata.
-include_recipe "nodejs::npm"
-include_recipe "pm2"
+include_recipe "apt"
+include_recipe "nodejs"
 
-
-nginx_install 'repo' do
-  group 'apache'
-  default_site_enabled true
+apt_update "update_sources" do
+  action :update
 end
 
-remote_directory '/home/ubuntu/app' do
+package "nginx"
+
+nodejs_npm "pm2"
+
+# nginx_install 'repo' do
+#   group 'apache'
+#   default_site_enabled true
+# end
+
+remote_directory '/home/vagrant/app' do
   source 'app'
   owner 'root'
   group 'root'
   mode '0777'
   recursive true
   action :create
+end
+
+service 'nginx' do
+  action :start
+end
+
+service 'nginx' do
+  action :enable
 end
