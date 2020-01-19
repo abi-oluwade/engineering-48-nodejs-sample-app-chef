439b359e7beb0bb36c945dc1f8b891d874b13d4f
Abi Oluwade <abi.oluwade1@gmail.com>
2020-01-19 12:50:25 +0000

    working app using chef syntax + bash script

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,25 +1,47 @@
-#
-# Cookbook:: nodejsapp
-# Recipe:: default
-#
-# Copyright:: 2020, The Authors, All Rights Reserved.
 
-# imports and installs the node.js dependencies and libraries specified in the metadata.
-include_recipe "apt"
-include_recipe "nodejs"
+# imports of recipes
+include_recipe 'apt'
+include_recipe 'nodejs'
 
-apt_update "update_sources" do
+#packages apt-get
+apt_update 'update_sources' do
   action :update
 end
 
-package "nginx"
+# package 'npm'
+package 'nginx'
 
-nodejs_npm "pm2"
 
-# nginx_install 'repo' do
-#   group 'apache'
-#   default_site_enabled true
-# end
+#npm Installs
+nodejs_npm 'pm2'
+
+
+# resource service
+service 'nginx' do
+  action :start
+end
+
+service 'nginx' do
+  action :enable
+end
+
+# resource template
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  variables proxy_port: node['nginx']['proxy_port']
+  notifies :restart, 'service[nginx]'
+end
+
+# resource link
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+  notifies :restart, 'service[nginx]'
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies :restart, 'service[nginx]'
+end
 
 remote_directory '/home/vagrant/app' do
   source 'app'
@@ -30,10 +52,12 @@ remote_directory '/home/vagrant/app' do
   action :create
 end
 
-service 'nginx' do
-  action :start
-end
-
-service 'nginx' do
-  action :enable
-end
+# bash 'start app' do
+#   user 'root'
+#   code <<-EOH
+# cd app/
+#sudo chown -R 1000:1000 /home/vagrant/.npm/
+# sudo npm install
+# npm start
+#     EOH
+# end
